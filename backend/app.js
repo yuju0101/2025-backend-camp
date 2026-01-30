a9147885d60fa876e4bf8465caf4966bf9fdc7f0
lauren <laurenweng@cw.com.tw>
2026-01-30 18:02:27 +0800

    ✨ feat: 修改健康檢查回傳格式 - 將回傳格式從純文字改為 JSON - 新增狀態與訊息欄位

--- backend/app.js
+++ backend/app.js
@@ -28,9 +28,12 @@ app.use(pinoHttp({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/healthcheck', (req, res) => {
-  res.status(200)
-  res.send('OK')
+  res.status(200).json({
+    status: 'success',
+    message: 'OK'
+  })
 })
+
 app.use('/api/credit-package', creditPackageRouter)
 app.use('/api/coaches/skill', skillRouter)
 app.use('/api/users', usersRouter)
