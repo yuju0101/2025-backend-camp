4f5be5fe37c715444e4a06ea33e9feee5b6b0983
lauren <laurenweng@cw.com.tw>
2026-01-30 17:46:36 +0800

    🐛 fix: 修改密碼長度限制 - 將密碼長度要求從 8-16 改為 10-16

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -76,7 +76,7 @@ class UsersController {
 
   static async postLogin (req, res, next) {
     try {
-      const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}/
+      const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,16}/
       const { email, password } = req.body
       if (isUndefined(email) || isNotValidSting(email) || isUndefined(password) || isNotValidSting(password)) {
         logger.warn('欄位未填寫正確')
